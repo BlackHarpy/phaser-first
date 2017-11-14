3725cf0d5700f303065c9dc5796818e48f628057
Maria Castro <mariacastro87@gmail.com>
2017-11-13 23:50:04 -0300

    'Hello World' text

--- src/game/app.ts
+++ src/game/app.ts
@@ -8,22 +8,33 @@ import 'phaser'
 class SimpleGame {
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create })
     }
 
     game: Phaser.Game;
 
     preload() {
-        this.game.load.image('logo', '../assets/phaser.png');
+        this.game.load.image('logo', '../assets/phaser.png')
     }
 
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo')
+
+        const textAlign = {
+            font: "15px Arial", 
+            fill: "#19de65" ,
+            align:  'left',
+            boundsAlignH: 'center',
+            boundsAlignV: 'bottom'
+        }
+        const text = this.game.add.text(0, 0, 'Hello World!', textAlign)
+        
+        text.setTextBounds(16, 16, 768, 568)
+        logo.anchor.setTo(0.5, 0.5)
     }
 
 }
 
 window.onload = () => {
-    var game = new SimpleGame();
+    const game = new SimpleGame()
 };
